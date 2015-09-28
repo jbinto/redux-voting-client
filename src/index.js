eeccd62e41fd944c3edabede227321252f67f8b2
Jesse Buchanan <hello@jessebuchanan.ca>
2015-09-28 00:49:33 -0400

    Update webpack.config to use Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Hello world');
+console.log(`Hello world, 2 plus 2 is ${2+2}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 // https://webpack.github.io/docs/configuration.html
-//
 
 module.exports = {
   entry: [
     './src/index.js'
   ],
+
+  // https://babeljs.io/docs/setup/#webpack
+  // https://github.com/babel/babel-loader
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  // https://webpack.github.io/docs/configuration.html#resolve-extensions
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
