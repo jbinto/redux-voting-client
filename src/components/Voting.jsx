59306f51f66d22c3f439fc74fc97350ffb6098e8
Jesse Buchanan <hello@jessebuchanan.ca>
2015-09-28 02:15:35 -0400

    Create Voting component

---
+++ src/components/Voting.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default React.createClass({
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  render: function() {
+    // JSX is weird, but "give it 5 minutes"
+    // 1) Cool: ES6 arrow function (expression only), that returns JSX
+    // 2) React concept: key
+    //    https://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}>
+          <h1>{entry}</h1>
+        </button>
+      )}
+    </div>;
+  }
+});
