8be8d008a44dd31abdf4c9d265da5fce5831bb90
Jesse Buchanan <hello@jessebuchanan.ca>
2015-11-28 22:03:47 -0500

    (Read comments) Pass action creator object to react-redux connect

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,6 +4,9 @@ import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
+// Imports all named exports from `action_creators` module
+// into an object named `actionCreators`.
+import * as actionCreators from '../action_creators';
 
 export const Voting = React.createClass({
   propTypes: {
@@ -46,4 +49,39 @@ function mapStateToProps(state) {
 //  the component that's being connected."
 //
 //  http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html#getting-data-in-from-redux-to-react
-export const VotingContainer = connect(mapStateToProps)(Voting);
+
+//  XXX CLEVER
+//  We also provide the "mapDispatchToProps" callback by directly importing
+//  the actionCreators module, which maps perfectly to the struture expected
+//  by react-redux connect. The action creators `setState` and `vote` are also
+//  callbacks on the <VotingContainer>.
+//
+//  XXX what about dispatch? This doesn't make any sense. Redux passes dispatch
+//  to the 2nd callback. actionCreators is an OBJECT, not a FUNCTION. WTF?
+//
+//  OK. Read the code.
+//   https://github.com/rackt/react-redux/blob/6c03cb78b337fa6fa705e18a1b369e83d097e42f/src/components/connect.js#L27
+//
+//  If `mapDispatchToProps` is a plain object, Redux calls `wrapActionCreators`,
+//  which returns a curried(?) function... calling `bindActionCreators`...
+//
+//  Wait, what about the docs?
+//
+//  `bindActionCreators` docs:
+//  http://rackt.org/redux/docs/api/bindActionCreators.html
+//
+//  Salient point: "[Returns a]n object mimicking the original object, but with
+//  each function immediately dispatching the action returned by the
+//  corresponding action creator."
+//
+// `connect` docs:
+// https://github.com/rackt/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
+//
+// "If an object is passed, each function inside it will be assumed to be a
+//  Redux action creator. An object with the same function names, but bound to
+//  a Redux store, will be merged into the component’s props."
+
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting);
